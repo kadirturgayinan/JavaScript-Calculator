4d2eeceb86bbd831005c4efe3ff50f6fda1714ff
kadirturgayinan <166447889+kadirturgayinan@users.noreply.github.com>
2024-05-13 04:21:41 +0200

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -8,32 +8,51 @@ var number2 = parseFloat(prompt("Enter the second number:"));
 var operation = prompt("Choose the operation: +, -, *, /");
 
 // Perform the selected operation
+
 var result;
+
 switch (operation) {
+        
     case "+":
+        
         // Addition
         result = number1 + number2;
         console.log("Sum: " + result);
+        
         break;
+        
     case "-":
+        
         // Subtraction
+        
         result = number1 - number2;
         console.log("Difference: " + result);
+        
         break;
+        
     case "*":
+        
         // Multiplication
+        
         result = number1 * number2;
         console.log("Product: " + result);
+        
         break;
+        
     case "/":
+        
         // Division
+        
         if (number2 === 0) {
             console.log("Error: Division by zero!");
+            
         } else {
             result = number1 / number2;
             console.log("Quotient: " + result);
+            
         }
         break;
+        
     default:
         console.log("Invalid operation!");
 }
